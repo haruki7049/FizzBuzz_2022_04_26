a00b71d335d42cf342d8971ec5caad5a0e042773
haruki7049 <tontonkirikiri@gmail.com>
2022-04-26 17:55:37 +0900

    とりあえずできた 15で割り切れた場合に”FizzBuzz”って表示したいがどうしたらいいのかわからないなぁ

---
+++ main.rb
@@ -0,0 +1,20 @@
+def fizz_buzz(n)
+    if n % 3 == 0
+        "Fizz"
+    elsif n % 5 == 0
+        "Buzz"
+    else
+        n.to_s
+    end
+end
+
+class MainUnko
+    def unko
+        for elem in 1..100 do
+            puts fizz_buzz(elem)
+        end
+    end
+end
+
+U = MainUnko.new
+U.unko()
